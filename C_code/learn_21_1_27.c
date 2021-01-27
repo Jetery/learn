12c723fe7773bd92ce9a6e735256ee5fa3a6892c
Jetery <981695266@qq.com>
2021-01-28 00:17:38 +0800

    补充

--- C_code/learn_21_1_27.c
+++ C_code/learn_21_1_27.c
@@ -200,4 +200,24 @@ int main()
 */
 
 
-
+//2
+//ตÝน้
+#include <stdio.h>
+int fib(int n)
+{
+	if ((n == 1) || (n == 2))
+	{
+		return 1;
+	}
+	if (n > 2)
+	{
+		return fib(n - 1) + fib(n - 2);
+	}
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d\n", fib(n));
+	return 0;
+}
