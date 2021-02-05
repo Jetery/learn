126dfeee405d67e785c627b965f4769f9c0662ea
Jetery <981695266@qq.com>
2021-02-05 22:12:27 +0800

    make GIF

---
+++ Python_code/GIF.py
@@ -0,0 +1,17 @@
+"""
+作者:Jetery
+日期:2020//12//30//
+"""
+from MyQR import myqr
+
+myqr.run(
+    words='Can I have your QQ?',
+    # 扫描二维码后，显示的内容，或是跳转的链接
+    version=5,  # 设置容错率
+    level='H',  # 控制纠错水平，范围是L、M、Q、H，从左到右依次升高
+    picture='E:\PycharmProjects\pythonProject1\L17-17-2019119164156.png',  # 图片所在目录，可以是动图
+    colorized=True,  # 黑白(False)还是彩色(True)
+    contrast=1.0,  # 用以调节图片的对比度，1.0 表示原始图片。默认为1.0。
+    brightness=1.0,  # 用来调节图片的亮度，用法同上。
+    save_name='Python.png',  # 控制输出文件名，格式可以是 .jpg， .png ，.bmp ，.gif
+    )
