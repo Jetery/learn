6af45f32e71ccd3dd4290e19a05748911cbd5a02
Jetery <981695266@qq.com>
2021-02-06 21:50:28 +0800

    little trick

---
+++ C_code/learn_21_2_6.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//实现函数init() 初始化数组为全0
+//实现print()  打印数组的每个元素
+//实现reverse()  函数完成数组元素的逆置
+#include <stdio.h>
+void init(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		arr[i] = 0;
+	}
+}
+void reverse(int arr[],int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		int temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+		left++;
+		right--;
+	}
+}
+void print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);
+	reverse(arr,sz);
+	print(arr, sz);
+	init(arr,sz);
+	print(arr, sz);
+	return 0;
+}
