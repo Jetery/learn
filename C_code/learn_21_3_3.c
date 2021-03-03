794e317f09a475a70ea5a89ab304c3f58d6b1b17
Jetery <981695266@qq.com>
2021-03-03 19:40:51 +0800

    异或操作符

---
+++ C_code/learn_21_3_3.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	//
+	//交换两个整形的方法
+	//
+
+	////1.
+	////创建临时变量
+	//int temp = a;
+	//a = b;
+	//b = temp;
+
+	////2.
+	////通过加减 - 问题 - 有溢出的可能
+	//a = a + b;
+	//b = a - b;
+	//a = a - b;
+
+	//3.
+	//异或操作符
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("a=%d b=%d\n", a, b);
+	return 0;
+}
