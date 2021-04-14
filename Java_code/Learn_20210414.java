88b5ecea6c142b9d5533cd14ecb51cb57d56d6c6
Jetery <981695266@qq.com>
2021-04-14 23:20:14 +0800

    Two-point lookup

---
+++ Java_code/Learn_20210414.java
@@ -0,0 +1,35 @@
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User: Jetery
+ * Date: 2021.04.14
+ */
+public class Learn_20210414 {
+
+    public static int find(int[] arr,int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target > arr[mid]) {
+                left = mid + 1;
+            } else if (target < arr[mid]){
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        //Two-point lookup
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        int ret = find(arr,2);
+        if (ret == -1) {
+            System.out.println("没找到");
+        } else {
+            System.out.println("找到了，下标为：" + ret);
+        }
+    }
+}
