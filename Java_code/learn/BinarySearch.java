a92d76422a3181ec44bfb562cbf0ff0c7722e3f8
Jetery <981695266@qq.com>
2021-06-09 00:09:34 +0800

    sort

---
+++ Java_code/learn/BinarySearch.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 如果目标值存在返回下标，否则返回 -1
+     * @param nums int整型一维数组 
+     * @param target int整型 
+     * @return int整型
+     */
+    public int search (int[] nums, int target) {
+        // write code here
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                while (mid != 0 && nums[mid-1] == nums[mid]) {
+                    mid--;
+                }
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
