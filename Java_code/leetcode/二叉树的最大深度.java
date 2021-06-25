cb396c5c9d5da0ac56f785aae518598cc8f966c6
Jetery <981695266@qq.com>
2021-06-25 23:18:51 +0800

    new way

--- Java_code/leetcode/二叉树的最大深度.java
+++ Java_code/leetcode/二叉树的最大深度.java
@@ -23,3 +23,38 @@ class Solution {
         return Math.max(leftDepth, rightDepth) + 1;
     }
 }
+
+
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
+
+//use BFS
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        if (root == null) return 0;
+        
+        int depth = 0;
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.offer(root);
+        while (!nodes.isEmpty()) {
+            int size = nodes.size();
+            depth++;
+            while (size-- > 0) {
+                TreeNode node = nodes.poll();
+                if (node.left != null) nodes.offer(node.left);
+                if (node.right != null) nodes.offer(node.right);
+            }
+        }
+        return depth;
+    }
+}
