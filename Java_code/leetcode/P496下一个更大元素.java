1c9ce766732b57f09479fc21504fecdbe42642ea
Jetery <981695266@qq.com>
2021-06-29 23:23:02 +0800

    增加了单调栈方法

--- Java_code/leetcode/P496下一个更大元素.java
+++ Java_code/leetcode/P496下一个更大元素.java
@@ -26,3 +26,21 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
