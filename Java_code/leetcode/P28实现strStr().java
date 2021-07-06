2b7d5cbc8b85749a2caf3410b3f8221047d055a8
Jetery <981695266@qq.com>
2021-07-06 23:53:44 +0800

    learn

--- Java_code/leetcode/P28实现strStr().java
+++ Java_code/leetcode/P28实现strStr().java
@@ -6,11 +6,11 @@ class Solution {
 
 
 public int strStr(String haystack, String needle) {
-  for (int i = 0; ; i++) {
-    for (int j = 0; ; j++) {
-      if (j == needle.length()) return i;
-      if (i + j == haystack.length()) return -1;
-      if (needle.charAt(j) != haystack.charAt(i + j)) break;
+  for (int i = 0; ; i++) { // The length of haystack
+    for (int j = 0; ; j++) { // The length of needle
+      if (j == needle.length()) return i; // If at this point we have navigated through the entire length of needle, we have found a solution, haystack[i].
+      if (i + j == haystack.length()) return -1; // This happens when we run out of elements in haystack, but there are still elements in needle. 
+      if (needle.charAt(j) != haystack.charAt(i + j)) break; // We stop comparing after needle[j], so i will be incremented and cycle repeats itself. 
     }
   }
 }
