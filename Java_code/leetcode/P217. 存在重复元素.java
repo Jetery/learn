8b59b3fdc671fc94f4fd8f07a4f26eafb327aa95
Jetery <981695266@qq.com>
2021-08-06 23:49:29 +0800

    two pointers

--- Java_code/leetcode/P217. 存在重复元素.java
+++ Java_code/leetcode/P217. 存在重复元素.java
@@ -11,3 +11,17 @@ class Solution {
         return false;
     }
 }
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0, j = 1; j < nums.length; ) {
+            if (nums[i] != nums[j]){
+                i++;
+                j++;
+            } else {return true;}
+        }
+        return false;
+    }
+}
