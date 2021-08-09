55c8cb704c95219105441ce90580fb024b38687a
Jetery <981695266@qq.com>
2021-08-09 23:40:01 +0800

    HashMap

--- Java_code/leetcode/P350. 两个数组的交集 II.java
+++ Java_code/leetcode/P350. 两个数组的交集 II.java
@@ -21,3 +21,35 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        //确保nums1是最短数组
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i : nums1) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(int i : nums2) {
+            if (map.getOrDefault(i, 0) > 0) {
+                list.add(i);
+                map.put(i, map.get(i) - 1);
+            }
+        }
+        int n = list.size();
+        int[] res = new int[n];
+        for(int i = 0; i < n; i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+
+    }
+}
