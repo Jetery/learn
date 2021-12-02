a1cd4f88445cd78ff4429ba604429595911b4e26
Jetery <981695266@qq.com>
2021-12-02 21:35:41 +0800

    daily LC

---
+++ Java_code/leetcode/P1446. 连续字符.java
@@ -0,0 +1,51 @@
+/*
+
+
+给你一个字符串 s ，字符串的「能量」定义为：只包含一种字符的最长非空子字符串的长度。
+
+请你返回字符串的能量。
+
+ 
+
+示例 1：
+
+输入：s = "leetcode"
+输出：2
+解释：子字符串 "ee" 长度为 2 ，只包含字符 'e' 。
+示例 2：
+
+输入：s = "abbcccddddeeeeedcba"
+输出：5
+解释：子字符串 "eeeee" 长度为 5 ，只包含字符 'e' 。
+示例 3：
+
+输入：s = "triplepillooooow"
+输出：5
+示例 4：
+
+输入：s = "hooraaaaaaaaaaay"
+输出：11
+示例 5：
+
+输入：s = "tourist"
+输出：1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/consecutive-characters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+class Solution {
+    public int maxPower(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); ){
+            int j = i + 1;
+            while (j < s.length() && s.charAt(j) == s.charAt(i)){
+                j++;
+            }
+            ans = Math.max(j - i, ans);
+            i = j;
+        }
+        return ans;       
+    }
+}
