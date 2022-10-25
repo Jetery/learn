526e99623c944b4c43b391570897363f626d2961
Jetery <981695266@qq.com>
2022-10-24 22:48:49 -0700

    quick_sort

---
+++ Cpp_code/acwing/alg_basic/fundamental/quick_sort.cpp
@@ -0,0 +1,30 @@
+/*
+ * Description: 
+ * @author: JeffRay
+ * @date: 2022.10.24
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+void quick_sort(int q[], int l, int r) {
+    if (l >= r) return;
+    int x = q[(l + r) >> 1], i = l - 1, j = r + 1;
+    while (i < j) {
+        do i++; while (q[i] < x);
+        do j--; while (q[j] > x);
+        if (i < j) swap(q[i], q[j]);
+    }
+    quick_sort(q, l, j);
+    quick_sort(q, j + 1, r);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int q[n];
+    for (int i = 0; i < n; i++) scanf("%d", &q[i]);
+    quick_sort(q, 0, n - 1);
+    for (int i = 0; i < n; i++) printf("%d ", q[i]);
+    return 0;
+}
