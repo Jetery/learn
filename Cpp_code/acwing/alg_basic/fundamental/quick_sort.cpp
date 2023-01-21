374cb1bfcb14ea4bd46df5fd9ddd596491d97b0a
Jetery <981695266@qq.com>
2023-01-20 22:22:14 -0800

    update quick_sort model

--- Cpp_code/acwing/alg_basic/fundamental/quick_sort.cpp
+++ Cpp_code/acwing/alg_basic/fundamental/quick_sort.cpp
@@ -11,8 +11,8 @@ void quick_sort(int q[], int l, int r) {
     if (l >= r) return;
     int x = q[(l + r) >> 1], i = l - 1, j = r + 1;
     while (i < j) {
-        do i++; while (q[i] < x);
-        do j--; while (q[j] > x);
+        while (q[ ++ i] < x);
+        while (q[ -- j] > x);
         if (i < j) swap(q[i], q[j]);
     }
     quick_sort(q, l, j);
